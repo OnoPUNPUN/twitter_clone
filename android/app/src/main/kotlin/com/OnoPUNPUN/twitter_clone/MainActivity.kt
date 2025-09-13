b899dc8fb72cbb7279e03aa09ff16382b239737f
OnoPUNPUN <punpunonodera754@gmail.com>
2025-09-13 10:05:05 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/OnoPUNPUN/twitter_clone/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.OnoPUNPUN.twitter_clone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,5 @@
+class AppwriteConstansts {
+  static const String databaseId = "68c4e87600365b71c885";
+  static const String projectId = "68c41e7b001c62e150dc";
+  static const String endPoint = "http://192.168.10.24/v1";
+}
