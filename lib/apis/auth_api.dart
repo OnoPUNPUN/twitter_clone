87b692ebbd70d1d0b0f96900498af40ac962b52c
OnoPUNPUN <punpunonodera754@gmail.com>
2025-09-20 19:33:21 +0600

    Created The API Functions To Create user account on appwrite dashboard

---
+++ lib/apis/auth_api.dart
@@ -0,0 +1,37 @@
+import 'package:appwrite/appwrite.dart';
+import 'package:appwrite/models.dart' as model;
+import 'package:fpdart/fpdart.dart';
+import 'package:twitter_clone/core/core.dart';
+import '../core/type_defs.dart';
+
+// want to sign Up want to get user account -> Account
+// want to access user related data -> model.User
+abstract class IAuthAPI {
+  FutureEither<model.User> signUp({
+    required String email,
+    required String password,
+  });
+}
+
+class AuthAPI implements IAuthAPI {
+  final Account _account;
+
+  AuthAPI({required Account account}) : _account = account;
+
+  @override
+  FutureEither<model.User> signUp({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final account = await _account.create(
+        userId: ID.unique(),
+        email: email,
+        password: password,
+      );
+      return right(account);
+    } catch (e, stackTrace) {
+      return left(Failure(e.toString(), stackTrace));
+    }
+  }
+}
